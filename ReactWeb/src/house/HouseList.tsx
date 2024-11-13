ab5f2191a3070e6ca57efcbd3102b460055f784a
Domingo <30532920+ctagayun@users.noreply.github.com>
2024-11-13 09:17:23 -0600

    Completed

--- ReactWeb/src/house/HouseList.tsx
+++ ReactWeb/src/house/HouseList.tsx
@@ -4,8 +4,10 @@ import { currencyFormatter } from "../config";
 import useFetchHouses from "../hooks/HouseHooks";
 
 const HouseList = () => {
-  //*Add this code. When a house is clicked you wiln routed to the HouseDetailComponent
- //* first go to tBody and add this code to <tr key=h.id> onClick={() => nav(`/house/${h.id}`)}
+  //* Add this code. When a house is clicked you wiln routed to the HouseDetailComponent
+  //* first go to tBody and add this code to <tr key=h.id> onClick={() => nav(`/house/${h.id}`)}
+  //* Calling useNavigate is different than doing a redirect. the function will not create 
+  //* a request to the server it will use the internal routing system
   const nav = useNavigate();
 
   //call the custom hook. In order to get to data
